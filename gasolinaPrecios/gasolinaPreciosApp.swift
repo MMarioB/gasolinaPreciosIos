3cef282bc1ca2cca1907fa7dd1a55928493e4498
MMarioB <mmariob196@gmail.com>
2025-01-23 14:10:27 +0100

    Initial Commit

---
+++ gasolinaPrecios/gasolinaPreciosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gasolinaPreciosApp.swift
+//  gasolinaPrecios
+//
+//  Created by Mario Bravo on 23/1/25.
+//
+
+import SwiftUI
+
+@main
+struct gasolinaPreciosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gasolinaPreciosTests/gasolinaPreciosTests.swift
@@ -0,0 +1,17 @@
+//
+//  gasolinaPreciosTests.swift
+//  gasolinaPreciosTests
+//
+//  Created by Mario Bravo on 23/1/25.
+//
+
+import Testing
+@testable import gasolinaPrecios
+
+struct gasolinaPreciosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
